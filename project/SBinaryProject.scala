6bcf6f5affe1ed213b935285f706e77a37f6d626
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2011-09-22 16:09:11 +0200

    Some changes necessary to push to ide repo and compile/publish for all the necessary scala versions

--- project/SBinaryProject.scala
+++ project/SBinaryProject.scala
@@ -3,30 +3,54 @@
 
 object SBinaryProject extends Build
 {
-	lazy val projects = Seq(root, core, treeExample)
+	override lazy val projects = Seq(root, core, treeExample)
 
 	lazy val root = Project("root", file(".")) settings( aux("SBinary Parent") : _*) aggregate(core, treeExample)
 	lazy val core = Project("core", file("core")) settings(coreSettings : _*)
 	lazy val treeExample = Project("examples", file("examples") / "bt") settings( aux("SBinary Tree Example") : _*) dependsOn(core)
 
+
 	lazy val commonSettings: Seq[Setting[_]] = Seq(
 		organization := "org.scala-tools.sbinary",
 		version := "0.4.0",
-		scalaVersion := "2.9.0",
-		crossScalaVersions += "2.8.1"
+		scalaVersion := "2.9.0-1",
+                resolvers += ScalaToolsSnapshots,
+		crossScalaVersions := Seq("2.8.1", "2.8.2-SNAPSHOT", "2.9.1","2.10.0-SNAPSHOT"),
+                publishMavenStyle := true
 	)
 
-	lazy val scalaCheck = libraryDependencies <+= scalaVersion { sv =>
-		val v = if(sv startsWith "2.9") "1.9" else "1.7"
-		"org.scala-tools.testing" %% "scalacheck" % v % "test"
-	}
+        def cutVersion(version: String): String = {
+          val pattern = "(\\d)\\.(\\d+)\\..*".r
+          version match {
+            case pattern(major, minor)=>
+              major + "." + minor
+            case _ =>
+              error("Invalid Scala version")
+              ""
+          }
+        }
+
+	//lazy val scalaCheck = libraryDependencies <+= scalaVersion { sv =>
+	//	val v = if(sv startsWith "2.9") "1.9" else "1.7"
+	//	"org.scala-tools.testing" %% "scalacheck" % v % "test"
+	//}
 	lazy val coreSettings = commonSettings ++ template ++ Seq(
 		name := "SBinary",
-		scalaCheck,
-		publishTo := Some("Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"),
-		credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
+		//scalaCheck,
+		//publishTo := Some("Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"),
+                publishTo in ThisBuild <<= publishResolver,
+		credentials += Credentials(Path.userHome / ".ivy2" / ".typesafe-credentials"),
 		unmanagedResources in Compile <+= baseDirectory map { _ / "LICENSE" }
-	)
+	)// ++ localPublishSettings
+/*        lazy val localPublishSettings = Seq(
+          otherResolvers += Resolver.file("some-id", file("/tmp/sbt/publish")), 
+          publishLocalConfiguration <<= (packagedArtifacts, deliverLocal, ivyLoggingLevel) map { 
+           (arts, _, level) => sbt.Classpaths.publishConfig(arts, None, resolverName = "some-id", logging = level ) 
+          }
+        )*/
+	def publishResolver: Project.Initialize[Option[Resolver]] = (scalaVersion) { (sv) =>
+          Some(Resolver.url("typesafe-ide-" + cutVersion(sv), url("https://typesafe.artifactoryonline.com/typesafe/ide-" + cutVersion(sv)))(Resolver.mavenStylePatterns))}
+
 	def aux(nameString: String) = commonSettings ++ Seq( publish := (), name := nameString )
 
 	/*** Templating **/
@@ -35,7 +59,8 @@ object SBinaryProject extends Build
 	lazy val fmppOptions = SettingKey[Seq[String]]("fmpp-options")
 	lazy val fmppConfig = config("fmpp") hide
 
-	lazy val template = fmppConfig(Test) ++ fmppConfig(Compile) ++ templateBase
+//	lazy val template = fmppConfig(Test) ++ fmppConfig(Compile) ++ templateBase
+	lazy val template = fmppConfig(Compile) ++ templateBase
 	lazy val templateBase = Seq(
 		libraryDependencies += "net.sourceforge.fmpp" % "fmpp" % "0.9.14" % fmppConfig.name,
 		ivyConfigurations += fmppConfig,
